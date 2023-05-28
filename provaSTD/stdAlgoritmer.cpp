ee55dc98703928f98b8cc22260a14b40d87caee8
Nasir2212 <Nasirjamaelmi@outlook.com>
2023-05-29 01:50:10 +0200

    update

--- provaSTD/stdAlgoritmer.cpp
+++ provaSTD/stdAlgoritmer.cpp
@@ -46,9 +46,117 @@ void provaTransorm2()
     skrivHorisontellt("efter transform:", vec3);
 
 }
+void provaIota(){
+    vector<int> vec(10);
+    skrivHorisontellt("prova iota A:", vec);
+    iota( begin(vec), end(vec), 42);
+    skrivHorisontellt("prova iota A:", vec);
+
+    vector<int> vecb(10);
+    skrivHorisontellt("prova iota B:", vecb);
+    iota(begin(vecb), end(vecb), 0);
+    skrivHorisontellt("prova iota B:", vecb);
+
+
+    string str('z'-'a'+1,' ');
+    skrivHorisontellt("prova iota string:", str);
+    iota(begin(str),end(str), 'a');
+    skrivHorisontellt("prova iota string:", str);
+}
+
+void provaGenerator_n()
+{
+    vector<int> fibsekvens;
+    auto ite = back_inserter(fibsekvens);
+    auto generator = [fore =0, forefore=0]() mutable
+    {
+        int e = forefore + fore; forefore=fore; fore=e; return e;
+    };
+    generate_n(ite, 10 , generator);
+    skrivHorisontellt("fibsekvens:",fibsekvens);
+}
+
+void provaGenerator_n2()
+{
+    vector<int> data;
+    auto ite = back_inserter(data);
+    auto generator = [x=0]() mutable
+    {
+        return x++;
+    };
+
+    generate_n(ite,10, generator);
+    skrivHorisontellt("fylld till 10:",data);
+
+}
+void provaSort()
+{
+    string str ="Here is a pretty large number of ASCII-characters.";
+
+    sort(begin(str), end(str));
+    cout <<"provaSort A:" <<str<<endl;
+
+    auto aForeB = [](char a, char b)    {return a>b;};
+    sort(begin(str), end(str), aForeB);
+    cout <<"provaSort B:" <<str<<endl;
+}
+
+struct AB{
+    int m_a;
+    int m_b;
+};
+ostream& operator <<(ostream &o, const AB &ab)
+{
+    o << "(" << ab.m_a << "," <<ab.m_b <<")";
+    return o;
+}
+
+void provaSortOchStableSort()
+{
+   vector <AB> data;
+   auto generateRandom = [] () {return AB{rand()%100, rand()%2};};
+   auto ite = back_inserter(data);
+   generate_n(ite,100,generateRandom);
+   skrivHorisontellt("före sort", data);
+
+
+   auto vilkorA = [](const AB &ab1, const AB &ab2) {return ab1.m_a<ab2.m_a;};
+   auto vilkorB = [](const AB &ab1, const AB &ab2) {return ab1.m_b<ab2.m_b;};
+
+   sort(begin(data), end(data), vilkorA);
+   skrivHorisontellt("efter sort A:", data);
+
+   stable_sort(begin(data), end(data), vilkorB);
+
+   skrivHorisontellt("efter sort B", data);
+}
+void provaRemoveIf()
+{
+    vector<int> data(10);
+    iota(begin(data),end(data),0);
+    skrivHorisontellt("före remove:",data);
+
+    auto vilkor = [] (int i) {return i<5;};
+    auto iteFrom = remove_if(begin(data), end(data), vilkor);
+    skrivHorisontellt("efter remove:",data);
+
+    data.erase(iteFrom,end(data));
+    skrivHorisontellt("Efter remove på riktigt",data);
+}
+
+
+
 
 void ingangTillStdAlgoritmer()
 {
    //provaTransorm1();
-    provaTransorm2();
+    //provaTransorm2();
+    //provaIota();
+    //provaGenerator_n();
+   // provaGenerator_n2();
+   // provaSort();
+    //provaSortOchStableSort();
+    provaRemoveIf();
+
+
 }
